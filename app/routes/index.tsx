c0165fea2a84f2e0189aa6d1de44f04f318281a4
Brian Cilenti <briancilenti@Brians-MacBook-Pro-2.local>
2022-02-05 10:38:35 -0500

    x

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -45,7 +45,7 @@ export default function Index() {
               <h3 className="text-sm font-medium text-yellow-800">
                 Attention needed
               </h3>
-              <div className="mt-2 text-sm text-yellow-700">
+              <div className="mt-2 text-sm text-yellow-700 py-7">
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Aliquid pariatur, ipsum similique veniam quo totam eius
@@ -56,6 +56,22 @@ export default function Index() {
           </div>
         </div>
       </ul>
+      <div className="bg-white">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">
+              Pricing
+            </h2>
+            <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
+              Take control of your team.
+            </p>
+            <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+              Start building for free, then add a site plan to go live. Account
+              plans unlock additional features.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
